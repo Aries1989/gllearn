1ccce82acad4150b8f833690fd603b2ae9097b29
sunjingzhao <sunjingzhao@megvii.com>
2020-10-14 18:49:31 +0800

    mod

--- gl_task01/main.cpp
+++ gl_task01/main.cpp
@@ -8,6 +8,8 @@
 
 int main()
 {
+	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+
 	glfwInit();
 	// 将主版本号(Major)和次版本号(Minor)都设为3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -17,7 +19,7 @@ int main()
 	// 不允许用户调整窗口的大小
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-
+	// 创建一个窗口对象
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
 	if (window == nullptr)
 	{
@@ -25,8 +27,36 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
+	// 将我们窗口的上下文设置为当前线程的主上下文
 	glfwMakeContextCurrent(window);
 
+
+	// 初始化glew
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	// glfwWindowShouldClose函数在我们每次循环的开始前检查一次GLFW是否被要求退出，
+	// 如果是的话该函数返回true然后游戏循环便结束了，之后为我们就可以关闭应用程序了。
+	while (!glfwWindowShouldClose(window))
+	{
+		// glfwPollEvents函数检查有没有触发什么事件（比如键盘输入、鼠标移动等），
+		// 然后调用对应的回调函数（可以通过回调方法手动设置）。我们一般在游戏循环的
+		// 开始调用事件处理函数。
+		glfwPollEvents();
+		// glfwSwapBuffers函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色
+		// 的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
