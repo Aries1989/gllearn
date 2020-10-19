e29456855730c3f782851de8c7d7086cdd377909
sunjingzhao <sunjingzhao@megvii.com>
2020-10-19 20:06:36 +0800

    mod

--- gl_task01/main.cpp
+++ gl_task01/main.cpp
@@ -1,15 +1,33 @@
+#include <iostream>
 // GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
 // GLFW
 #include <GLFW/glfw3.h>
 
-#include <iostream>
+void key_callback(GLFWwindow* window, int key, int scancode, 
+	int action, int mode);
+
+// 顶点着色器
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+// 片段着色器
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"}\n\0";
 
 int main()
 {
 	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
 
+	// 初始化GLFW
 	glfwInit();
 	// 将主版本号(Major)和次版本号(Minor)都设为3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -30,6 +48,8 @@ int main()
 	// 将我们窗口的上下文设置为当前线程的主上下文
 	glfwMakeContextCurrent(window);
 
+	// 注册函数至合适的回调
+	glfwSetKeyCallback(window, key_callback);
 
 	// 初始化glew
 	glewExperimental = GL_TRUE;
@@ -39,9 +59,97 @@ int main()
 		return -1;
 	}
 
+	// 定义视口尺寸
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
+
+	// 以线框模式绘制
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// 定义并编译顶点着色器
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// 检查编译时错误
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// 定义并编译片段着色器
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// 检查编译时错误
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// 定义并链接着色器
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// 检查链接时错误
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	// 删除着色器
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// 设置顶点和索引
+	GLfloat vertices[] = {
+		0.5f,  0.5f, 0.0f,  // Top Right
+		0.5f, -0.5f, 0.0f,  // Bottom Right
+		-0.5f, -0.5f, 0.0f,  // Bottom Left
+		-0.5f,  0.5f, 0.0f,   // Top Left 
+		-0.25f, 0.75f, 0.0f
+	};
+	GLuint indices[] = {  // Note that we start from 0!
+		0, 1, 3,  // First Triangle
+		1, 2, 3,  // Second Triangle
+		2, 3, 4
+	};
+
+	// 定义顶点缓冲对象、顶点数组对象，
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// 1. 绑定VAO
+	// 绑定顶点数组对象
+	glBindVertexArray(VAO);
+	// 2. 把顶点数组复制到缓冲中供OpenGL使用
+	// 绑定顶点缓冲对象
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
+	// 绑定索引缓冲对象
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	// 3. 设定顶点属性指针
+	// 指定渲染时索引值为0的顶点属性数组的数据格式和位置。
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 取消绑定的顶点缓冲对象
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// 4. 解绑VAO（不是EBO！）
+	// 取消绑定的顶点数组对象。
+	// Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), 
+	// remember: do NOT unbind the EBO, keep it bound to this VAO
+	glBindVertexArray(0);
 
 	// glfwWindowShouldClose函数在我们每次循环的开始前检查一次GLFW是否被要求退出，
 	// 如果是的话该函数返回true然后游戏循环便结束了，之后为我们就可以关闭应用程序了。
@@ -51,12 +159,37 @@ int main()
 		// 然后调用对应的回调函数（可以通过回调方法手动设置）。我们一般在游戏循环的
 		// 开始调用事件处理函数。
 		glfwPollEvents();
+
+		// 渲染
+		// 清理颜色缓冲
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 绘制第一个三角形
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
 		// glfwSwapBuffers函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色
 		// 的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
 		glfwSwapBuffers(window);
 	}
+	// 一旦不再使用，就释放所有的资源
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
+	// 终止GLFW，清理GLFW申请的所有资源
 	glfwTerminate();
 
 	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
+	// 关闭应用程序
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
